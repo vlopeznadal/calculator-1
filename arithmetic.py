687eab7e379b6cd38d8cafa073bfe9809824a5bb
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-09-29 11:39:12 -0500

    Completed add_mult function

--- arithmetic.py
+++ arithmetic.py
@@ -36,3 +36,10 @@ def power(num1, num2):
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
     return num1%num2
+
+def add_mult(num1, num2, num3):
+    """Get the sum of num1 and num2, then multiply sum with num3."""
+    return (num1 + num2) * num3
+
+def add_cubes(num1, num2):
+    """Add the cubes of num1 and num2."""
