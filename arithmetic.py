8a8fa783f0c6b264b475a78b4c629667a9aa4fe4
Virginia Lopez Nadal <vlopeznadal@icloud.com>
2021-09-29 11:06:55 -0500

    completed multiplication function

--- arithmetic.py
+++ arithmetic.py
@@ -15,7 +15,8 @@ def subtract(num1, num2):
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
-
+    return num1*num2
+    
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
